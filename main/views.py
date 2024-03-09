72f69b34fa0ccafb78c237b0955bbb1f3dc2863a
nnnalyd <84059521+nnnalyd@users.noreply.github.com>
2024-03-09 19:53:21 +1100

    commit updates

--- main/views.py
+++ main/views.py
@@ -4,8 +4,11 @@ import getToken
 import json
 
 # Create your views here.
+
+#displaying the api requests
 def displayAPI(request):
     token = getToken.get_token()
     response  = getToken.getArtist(token)
-    response_str = response["images"]
-    return render(request, 'show.html', {'data' : response_str})
+    response_str = response["name"]
+    response_img= response["images"][2]["url"]
+    return render(request, 'show.html', {'image' : response_img, 'data' : response_str})
